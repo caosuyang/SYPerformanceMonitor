b7a76bbf1e029830accf77f208ef7d95229e190e
caosuyang <caosuyang1026@gmail.com>
2023-03-09 23:01:23 +0800

    create SYLossTimeMonitor

---
+++ SYPerformanceMonitor/SYPerformanceMonitor/SYLossTimeMonitor.h
@@ -0,0 +1,17 @@
+//
+//  SYLossTimeMonitor.h
+//  SYPerformanceMonitor
+//
+//  Created by 曹素洋 on 2023/3/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// 耗时检查工具
+@interface SYLossTimeMonitor : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SYPerformanceMonitor/SYPerformanceMonitor/SYLossTimeMonitor.m
@@ -0,0 +1,12 @@
+//
+//  SYLossTimeMonitor.m
+//  SYPerformanceMonitor
+//
+//  Created by 曹素洋 on 2023/3/9.
+//
+
+#import "SYLossTimeMonitor.h"
+
+@implementation SYLossTimeMonitor
+
+@end
