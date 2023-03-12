cdf6769a1dbf912a88508fa5294ba9dd5158d2a1
caosuyang <caosuyang1026@gmail.com>
2023-03-12 09:17:24 +0800

    update SYLossTimeCore

--- SYPerformanceMonitor/SYPerformanceMonitor/StartupSpeed/SYLossTimeCore.c
+++ SYPerformanceMonitor/SYPerformanceMonitor/StartupSpeed/SYLossTimeCore.c
@@ -10,9 +10,32 @@
 #include <pthread/pthread.h>
 #include <dispatch/dispatch.h>
 #include "fishhook.h"
+#include <sys/time.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <stddef.h>
+//#include <stdint.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
+//#include <sys/time.h>
+//#include <objc/message.h>
+//#include <objc/runtime.h>
+//#include <dispatch/dispatch.h>
+//#include <pthread.h>
 
 #pragma mark - static
 __unused static id (*orig_objc_msgSend)(id, SEL, ...);
+static bool _call_record_enabled = true;
+static uint64_t _min_time_cost = 1000; //us
+static int _max_call_depth = 3;
+static pthread_key_t _thread_key;
+
+static call_record *_syCallRecords;
+//static int otp_record_num;
+//static int otp_record_alloc;
+static int _syRecordNum;
+static int _syRecordAlloc;
 
 #pragma mark - struct
 // thread_call_record 记录调用方法详细信息，包括 obj 和 SEL 等
@@ -32,34 +55,95 @@ typedef struct {
     bool is_main_thread;
 } thread_call_stack;
 
-#pragma mark - private methods
-/// objc_msgSend方法执行前
-void before_objc_msgSend(id self, SEL _cmd, uintptr_t lr) {
-    
-}
-
-/// objc_msgSend方法执行后
-uintptr_t after_objc_msgSend() {
-    return 0;
-}
-
 #pragma mark - core methods
+// fix: Static declaration of 'push_call_record' follows non-static declaration
+static inline thread_call_stack * get_thread_call_stack() {
+    thread_call_stack *cs = (thread_call_stack *)pthread_getspecific(_thread_key);
+    if (cs == NULL) {
+        cs = (thread_call_stack *)malloc(sizeof(thread_call_stack));
+        cs->stack = (thread_call_record *)calloc(128, sizeof(thread_call_record));
+        cs->allocated_length = 64;
+        cs->index = -1;
+        cs->is_main_thread = pthread_main_np();
+        pthread_setspecific(_thread_key, cs);
+    }
+    return cs;
+}
+
 /// 用于记录 objc_msgSend 方法调用前的时间，表示开始时
 static inline void push_call_record(id _self, Class _cls, SEL _cmd, uintptr_t lr) {
-    
+    thread_call_stack *cs = get_thread_call_stack();
+    if (cs) {
+        int nextIndex = (++cs->index);
+        if (nextIndex >= cs->allocated_length) {
+            cs->allocated_length += 64;
+            cs->stack = (thread_call_record *)realloc(cs->stack, cs->allocated_length * sizeof(thread_call_record));
+        }
+        thread_call_record *newRecord = &cs->stack[nextIndex];
+        newRecord->self = _self;
+        newRecord->cls = _cls;
+        newRecord->cmd = _cmd;
+        newRecord->lr = lr;
+        if (cs->is_main_thread && _call_record_enabled) {
+            struct timeval now;
+            gettimeofday(&now, NULL);
+            newRecord->time = (now.tv_sec % 100) * 1000000 + now.tv_usec;
+        }
+    }
 }
 
 /// 用于记录 objc_msgSend 方法调用后的时间，表示结束时
 /// pop_call_record - push_call_record 相减能够得到方法的执行耗时
 static inline uintptr_t pop_call_record() {
-    return 0;
+    thread_call_stack *cs = get_thread_call_stack();
+    int curIndex = cs->index;
+    int nextIndex = cs->index--;
+    thread_call_record *pRecord = &cs->stack[nextIndex];
+    
+    if (cs->is_main_thread && _call_record_enabled) {
+        struct timeval now;
+        gettimeofday(&now, NULL);
+        uint64_t time = (now.tv_sec % 100) * 1000000 + now.tv_usec;
+        if (time < pRecord->time) {
+            time += 100 * 1000000;
+        }
+        uint64_t cost = time - pRecord->time;
+        if (cost > _min_time_cost && cs->index < _max_call_depth) {
+            if (!_syCallRecords) {
+                _syRecordAlloc = 1024;
+                _syCallRecords = malloc(sizeof(call_record) * _syRecordAlloc);
+            }
+            _syRecordNum++;
+            if (_syRecordNum >= _syRecordAlloc) {
+                _syRecordAlloc += 1024;
+                _syCallRecords = realloc(_syCallRecords, sizeof(call_record) * _syRecordAlloc);
+            }
+            call_record *log = &_syCallRecords[_syRecordNum - 1];
+            log->cls = pRecord->cls;
+            log->depth = curIndex;
+            log->sel = pRecord->cmd;
+            log->time = cost;
+        }
+    }
+    return pRecord->lr;
 }
 
 static void release_thread_call_stack(void *ptr) {
-//    thread_call_stack *cs = (thread_call_stack *)ptr;
-//    if (!cs) return;
-//    if (cs->stack) free(cs->stack);
-//    free(cs);
+    thread_call_stack *cs = (thread_call_stack *)ptr;
+    if (!cs) return;
+    if (cs->stack) free(cs->stack);
+    free(cs);
+}
+
+#pragma mark - private methods
+/// objc_msgSend方法执行前
+void before_objc_msgSend(id self, SEL _cmd, uintptr_t lr) {
+    push_call_record(self, objc_getClass((const char * _Nonnull) self), _cmd, lr);
+}
+
+/// objc_msgSend方法执行后
+uintptr_t after_objc_msgSend() {
+    return pop_call_record();
 }
 
 #pragma mark - replacement objc_msgSend (arm64)
@@ -140,17 +224,24 @@ static void hook_Objc_msgSend() {
 }
 
 #pragma mark - public methods
-void start_monitor() {
-//    _call_record_enabled = true;
-//    static dispatch_once_t onceToken;
-//    dispatch_once(&onceToken, ^{
-//        pthread_key_create(&_thread_key, &release_thread_call_stack);
-//        rebind_symbols((struct rebinding[6]){
-//            {"objc_msgSend", (void *)hook_Objc_msgSend, (void **)&orig_objc_msgSend},
-//        }, 1);
-//    });
+void start_monitor(void) {
+    _call_record_enabled = true;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        pthread_key_create(&_thread_key, &release_thread_call_stack);
+        rebind_symbols((struct rebinding[6]){
+            {"objc_msgSend", (void *)hook_Objc_msgSend, (void **)&orig_objc_msgSend},
+        }, 1);
+    });
 }
 
-void end_monitor() {
-//    _call_record_enabled = false;
+void end_monitor(void) {
+    _call_record_enabled = false;
+}
+
+call_record *get_call_records(int *num) {
+    if (num) {
+        *num = _syRecordNum;
+    }
+    return _syCallRecords;
 }
--- SYPerformanceMonitor/SYPerformanceMonitor/StartupSpeed/SYLossTimeCore.h
+++ SYPerformanceMonitor/SYPerformanceMonitor/StartupSpeed/SYLossTimeCore.h
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <objc/objc.h>
 
+/// 调用记录struct
 typedef struct {
     __unsafe_unretained Class cls;
     SEL sel;
@@ -19,7 +20,9 @@ typedef struct {
 } call_record;
 
 /// 开始监听
-extern void start_monitor();
+extern void start_monitor(void);
 /// 结束监听
-extern void end_monitor();
+extern void end_monitor(void);
+/// 获取调用记录
+extern call_record *get_call_records(int *num);
 #endif /* SYLossTimeCore_h */
